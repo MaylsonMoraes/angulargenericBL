f51c9589737f95faee5ef58d4c2b85fcb8e889ee
MaylsonMoraes <maylsonmoraesd@hotmail.com>
2022-06-26 11:39:18 -0300

    teste de implementação no component

--- angularBL/src/app/components/generic-bl-component/generic-bl-component.component.ts
+++ angularBL/src/app/components/generic-bl-component/generic-bl-component.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-generic-bl-component',
   templateUrl: './generic-bl-component.component.html',
-  styleUrls: ['./generic-bl-component.component.css']
+  styleUrls: ['./generic-bl-component.component.css'],
 })
 export class GenericBLComponentComponent implements OnInit {
+  name: string = 'Maylson';
+  age: number = 33;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
